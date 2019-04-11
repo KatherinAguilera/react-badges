cd0b69b333dd7f64ff28dfbdb63ad7fa9426e14d
Katherine Aguilera <katherinam35@gmail.com>
2019-04-11 12:13:10 -0500

    Ciclo de vida

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,53 +1,89 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg';
-// import Navbar from '../components/Navbar';
 import BadgesList from '../components/BadgesList';
 
-import { Link } from 'react-router-dom';
-
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-        firstName: 'Freda',
-        lastName: 'Grady',
-        email: 'Leann_Berge@gmail.com',
-        jobTitle: 'Legacy Brand Director',
-        twitter: 'FredaGrady22221-7573',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-      },
-      {
-        id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-        firstName: 'Major',
-        lastName: 'Rodriguez',
-        email: 'Ilene66@hotmail.com',
-        jobTitle: 'Human Research Architect',
-        twitter: 'MajorRodriguez61545',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-      },
-      {
-        id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-        firstName: 'Daphney',
-        lastName: 'Torphy',
-        email: 'Ron61@hotmail.com',
-        jobTitle: 'National Markets Officer',
-        twitter: 'DaphneyTorphy96105',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-      },
-    ],
-  };
+  /************CICLO DE VIDA **************/
+  //Metodos constructor ocurre 1
+  constructor(props) {
+    // inicializar super clase
+    super(props);
+    console.log('1. constructor()');
 
+    this.state = {
+      data: [],
+    };
+  }
+// Monatje
+  componentDidMount() {
+    console.log('3. componentDidMount()');
+    // funcion asincrona para mostar el estado
+    this.timeoutId = setTimeout(() => {
+      this.setState({
+        data: [
+          {
+            id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+            firstName: 'Freda',
+            lastName: 'Grady',
+            email: 'Leann_Berge@gmail.com',
+            jobTitle: 'Legacy Brand Director',
+            twitter: 'FredaGrady22221-7573',
+            avatarUrl:
+              'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
+          },
+          {
+            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+            firstName: 'Major',
+            lastName: 'Rodriguez',
+            email: 'Ilene66@hotmail.com',
+            jobTitle: 'Human Research Architect',
+            twitter: 'MajorRodriguez61545',
+            avatarUrl:
+              'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
+          },
+          {
+            id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+            firstName: 'Daphney',
+            lastName: 'Torphy',
+            email: 'Ron61@hotmail.com',
+            jobTitle: 'National Markets Officer',
+            twitter: 'DaphneyTorphy96105',
+            avatarUrl:
+              'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
+          },
+        ],
+      });
+    }, 3000);
+  }
+  // Actualizacion
+// props anteriores/ estado que tenia antes
+  componentDidUpdate(prevProps, prevState) {
+    console.log('5. componentDidUpdate()');
+    console.log({
+      prevProps: prevProps,
+      prevState: prevState,
+    });
+
+    console.log({
+      props: this.props,
+      state: this.state,
+    });
+  }
+  // Desmontaje
+// que salga de la UI
+  componentWillUnmount() {
+    console.log('6. componentWillUnmount');
+    // funcion para limiar memoria
+    clearTimeout(this.timeoutId);
+  }
+  /************FIN CICLO DE VIDA **************/
   render() {
+    console.log('2/4. render()');
     return (
-      <div>
-        {/* <Navbar /> */}
-
+      <React.Fragment>
         <div className="Badges">
           <div className="Badges__hero">
             <div className="Badges__container">
@@ -62,18 +98,14 @@ class Badges extends React.Component {
 
         <div className="Badges__container">
           <div className="Badges__buttons">
-            {/* <a href="/badges/new" className="btn btn-primary">
-              New Badge
-            </a> */}
-            {/* uso de react-router-dom en el boton */}
             <Link to="/badges/new" className="btn btn-primary">
               New Badge
             </Link>
           </div>
-          {/* lista de elementos */}
+
           <BadgesList badges={this.state.data} />
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
