427cfa7aa10ee52f28b06b42e3590dff529c2446
Katherine Aguilera <katherinam35@gmail.com>
2019-04-12 16:42:14 -0500

    Enviando datos POST

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
+import header from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
 // import Navbar from '../components/Navbar';
-
+import api from '../api';
 class BadgeNew extends React.Component {
   state = {
     // inicializar el estado
@@ -17,7 +17,18 @@ class BadgeNew extends React.Component {
       twitter: '',
     },
   };
+  // enviar a la api
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true, error: null });
 
+    try {
+      await api.badges.create(this.state.form);
+      this.setState({ loading: false });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
   handleChange = e => {
     this.setState({
       form: {
@@ -33,19 +44,27 @@ class BadgeNew extends React.Component {
       <React.Fragment>
         {/* <Navbar /> */}
         <div className="BadgeNew__hero">
-          <img className="img-fluid" src={header} alt="Logo" />
+          {/* <img
+            className="BadgeNew_hero-image img-fluid"
+            src={header}
+            alt="Logo" /> */}
+          <img
+            className="BadgeNew__hero-image img-fluid"
+            src={header}
+            alt="Logo"
+          />
         </div>
 
         <div className="container">
           <div className="row">
             <div className="col-6">
               <Badge
-              // cambiar el estado de acuerdo al BadgeForm
-                firstName={this.state.form.firstName}
-                lastName={this.state.form.lastName}
-                twitter={this.state.form.twitter}
-                jobTitle={this.state.form.jobTitle}
-                email={this.state.form.email}
+              // cambiar el estado de acuerdo al BadgeForm || valor provicional
+                firstName={this.state.form.firstName || 'First_Name'}
+                lastName={this.state.form.lastName|| 'Last_Name'}
+                twitter={this.state.form.twitter || 'twitter'}
+                jobTitle={this.state.form.jobTitle || 'Job_Title'}
+                email={this.state.form.email || 'EMAIL'}
                 avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
               />
             </div>
@@ -53,6 +72,7 @@ class BadgeNew extends React.Component {
             <div className="col-6">
               <BadgeForm
                 onChange={this.handleChange}
+                onSubmit={this.handleSubmit}
                 formValues={this.state.form}
               />
             </div>
