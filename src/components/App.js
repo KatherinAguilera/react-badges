0a9b0146276c90c0faec8d8728331b63c131e760
Katherine Aguilera <katherinam35@gmail.com>
2019-04-15 11:44:59 -0500

    Badge Details

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from './Layout';
 import BadgeNew from '../pages/BadgeNew';
+import BadgeDetails from '../pages/BadgeDetails';
 import Badges from '../pages/Badges';
 import NotFound from '../pages/NotFound';
 import Home from '../pages/Home'
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
