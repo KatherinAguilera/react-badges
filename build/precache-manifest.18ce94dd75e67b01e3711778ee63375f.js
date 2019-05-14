ab2acd71d95be0e3d70d2e19373826329c556770
Katherine Aguilera <katherinam35@gmail.com>
2019-05-14 12:18:45 -0500

    Deploy

---
+++ build/precache-manifest.18ce94dd75e67b01e3711778ee63375f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5d31e41d21899c736e6",
+    "url": "/react-badges/static/css/main.6e045103.chunk.css"
+  },
+  {
+    "revision": "c5d31e41d21899c736e6",
+    "url": "/react-badges/static/js/main.c5d31e41.chunk.js"
+  },
+  {
+    "revision": "1ca7e770b323ef47f898",
+    "url": "/react-badges/static/js/runtime~main.1ca7e770.js"
+  },
+  {
+    "revision": "badcc1eec45546f5abb6",
+    "url": "/react-badges/static/css/2.1d2c60b6.chunk.css"
+  },
+  {
+    "revision": "badcc1eec45546f5abb6",
+    "url": "/react-badges/static/js/2.badcc1ee.chunk.js"
+  },
+  {
+    "revision": "b7ec03813cb02a32a24238da6b79ed62",
+    "url": "/react-badges/static/media/astronaut.b7ec0381.png"
+  },
+  {
+    "revision": "db47f06c0e1b8d994cb1cb993a6e1d78",
+    "url": "/react-badges/static/media/conference-icon.db47f06c.png"
+  },
+  {
+    "revision": "7e7abb160f737b4ef66a1daf61378446",
+    "url": "/react-badges/static/media/cohete.7e7abb16.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-badges/static/media/starts.1d17360a.svg"
+  },
+  {
+    "revision": "60142c99d4f5757af075a06b86d81dfe",
+    "url": "/react-badges/index.html"
+  }
+];
